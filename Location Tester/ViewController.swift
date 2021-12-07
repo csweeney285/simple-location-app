c6be240abfd49ca885d047f896fd888c8d84ad60
Conor Sweeney <conorsweeney1@gmail.com>
2021-12-07 13:50:32 -0500

    Initial Commit

---
+++ Location Tester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location Tester
+//
+//  Created by Conor Sweeney on 12/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
